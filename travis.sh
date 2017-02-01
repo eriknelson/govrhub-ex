12e4679cdeb29fb1b825c81fb3602b8c86d2cf06
Erik Nelson <erik@nsk.io>
2017-01-31 19:32:13 -0500

    newtest

--- travis.sh
+++ travis.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 if [[ "$1" == "install" ]]; then
   sudo apt-get install -y python-pip python-dev build-essential git
-  git checkout https://github.com/eriknelson/govr.git $HOME/govr
-  pip install requests
+  git clone https://github.com/eriknelson/govr.git $HOME/govr
+  sudo pip install requests
 elif [[ "$1" == "test" ]]; then
-  export GOVR_PROJECT_ROOT=$GOPATH/src/eriknelson/govrhub-ex
+  export GOVR_PROJECT_ROOT=$GOPATH/src/github.com/eriknelson/govrhub-ex
   python $HOME/govr/main.py
 fi
