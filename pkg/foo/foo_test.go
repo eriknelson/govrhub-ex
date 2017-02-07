275e076deca49918a4ab8d031564fc97718aec51
Erik Nelson <erik@nsk.io>
2017-02-06 21:59:43 -0500

    At least one test

--- pkg/foo/foo_test.go
+++ pkg/foo/foo_test.go
@@ -4,12 +4,12 @@ import (
 	"testing"
 )
 
-//func TestFoo(t *testing.T) {
-//value := Hello()
-//if value != "Foo::Hello" {
-//t.Fatal("Got unexpected value for Foo::Hello")
-//}
-//}
+func TestFoo(t *testing.T) {
+	value := Hello()
+	if value != "Foo::Hello" {
+		t.Fatal("Got unexpected value for Foo::Hello")
+	}
+}
 
 //func TestWorld(t *testing.T) {
 //value := World()
