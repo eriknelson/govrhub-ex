89f9a9340e63af757f5b48c736d9fa336d75c275
Erik Nelson <erik@nsk.io>
2017-02-06 21:57:20 -0500

    No testing!

--- pkg/foo/foo_test.go
+++ pkg/foo/foo_test.go
@@ -4,19 +4,19 @@ import (
 	"testing"
 )
 
-func TestFoo(t *testing.T) {
-	value := Hello()
-	if value != "Foo::Hello" {
-		t.Fatal("Got unexpected value for Foo::Hello")
-	}
-}
+//func TestFoo(t *testing.T) {
+//value := Hello()
+//if value != "Foo::Hello" {
+//t.Fatal("Got unexpected value for Foo::Hello")
+//}
+//}
 
-func TestWorld(t *testing.T) {
-	value := World()
-	if value != "Foo::World" {
-		t.Fatal("Got unexpected value for Foo::World")
-	}
-}
+//func TestWorld(t *testing.T) {
+//value := World()
+//if value != "Foo::World" {
+//t.Fatal("Got unexpected value for Foo::World")
+//}
+//}
 
 //func TestBaz(t *testing.T) {
 //value := Baz()
